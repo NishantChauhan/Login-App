6a2db1cd88f51f6b536d9df7f365d0bd238eeb0f
Nishant <Nishant>
2018-11-12 09:10:07 -0500

    Ng7 upgrade and Reactive Form Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import {FormsModule} from '@angular/forms';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { RegexValidatorDirective } from './regex-validator.directive';
+import { UserReactiveFormComponent } from './user-reactive-form/user-reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserDetailsComponent,
     UserFormComponent,
-    RegexValidatorDirective
+    RegexValidatorDirective,
+    UserReactiveFormComponent
   ],
   imports: [
     BrowserModule, FormsModule
